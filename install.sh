43966b82f3d9eb470ae26a421f1a3739d9b83204
Rizal Martin <49640119+rizalmart@users.noreply.github.com>
2023-11-23 07:14:45 +0800

    Create install.sh
    Added install.sh script since unable to create meson for the meantime

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#gTock script installer.
+
+if [ "$(whoami)" != "root" ]; then
+ exec sudo $0
+fi
+
+prompt_yes_no() {
+    while true; do
+        read -p "$1 (yes/no): " answer
+        case $answer in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+
+if prompt_yes_no "This installer will install gTock. Do you want to proceed?"; then
+    echo "Installing gTock...."
+else
+    echo "Installation cancelled"
+    exit 1
+fi
+
+
+script_dir=$(realpath $(dirname "$0"))
+
+if [ ! -d $script_dir/gtock-src ]; then
+ echo "gtock source files was missing"
+ exit 1
+fi
+
+cp -rf $script_dir/gtock-src/* /
+
+glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+echo "gTock installation complete!
+
