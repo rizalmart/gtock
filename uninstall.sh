5def5161c7ea29a5eb10fbc71bea789989e8e8e6
Rizal Martin <49640119+rizalmart@users.noreply.github.com>
2023-11-23 07:22:03 +0800

    Create uninstall.sh
    A script for removing gtock from system

---
+++ uninstall.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#gTock script installer.
+
+if [ "$(whoami)" != "root" ]; then
+ exec sudo $0
+fi
+
+prompt_yes_no() {
+    while true; do
+        read -p "$1 (yes/no): " answer
+        case $answer in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+
+if prompt_yes_no "This installer will remove gTock. Do you want to proceed?"; then
+    echo "Removing gTock...."
+else
+    echo "Uninstallation cancelled"
+    exit 1
+fi
+
+
+script_dir=$(realpath $(dirname "$0"))
+
+for rfile in gtock gtock-import gtock-export
+do
+ rm -f /usr/bin/${rfile}
+done
+
+rm -rf /usr/share/gtock
+rm -f /usr/share/applications/gtock.desktop
+rm -f /usr/share/glib-2.0/schemas/org.gtk.gtock.gschema.xml
+
+glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+echo "gTock uninstallation complete!
